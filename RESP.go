e6fae0da9b02d91e2ed0094eb907d16e1e067d73
Mohamed-khattab <mohamed.e.khattab.0@gmail.com>
2023-11-05 15:56:22 +0200

    add a resep file

---
+++ RESP.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const (
+	STRING = '+'
+	ERROR = '+'
+	INTEGER = ':'
+	BULK = '$'
+	ARRAY = '*'
+)
+
+type Value struct {
+	typ string // refare to data type 
+	str string  // strings from simple strings 
+	num int  
+	bulk string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r * Resp) readLine() (line []byte , n int , err error){
+	for {
+		b , err := r.reader.ReadByte()
+		if  err !=nil {
+			return nil , 0 , err
+		}
+		n +=1 
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r'{
+			break
+		}
+	}
+	return line[:len(line)-2] , n, nil
+}
