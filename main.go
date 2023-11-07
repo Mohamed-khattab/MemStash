40bbc69ad464725374a021810be5cf2207d1b5e7
Mohamed-khattab <mohamed.e.khattab.0@gmail.com>
2023-11-07 16:19:00 +0200

    update thye engine

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
@@ -22,19 +20,13 @@ func main() {
 	defer conn.Close() // close the connection once finished
 
 	for {
-		// init a buffer to hold the incoming date from network connection
-		buf := make([]byte, 1024)
-		// read the data from the connection
-		_, err = conn.Read(buf)
-
+		resp :=  NewResp(conn)
+		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("Error reading from client ", err.Error())
-			os.Exit(1)
+			fmt.Println(err)
 			return
 		}
+		fmt.Println(value)
 
 		_, err = conn.Write([]byte("+OK\r\n"))
 		if err != nil {
