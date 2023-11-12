0aefca98b2e531f0c483787455697ea2ac6ab4e1
Mohamed-khattab <mohamed.e.khattab.0@gmail.com>
2023-11-12 12:47:49 +0200

    add ping command handerler

--- handler.go
+++ handler.go
@@ -1,19 +1,8 @@
-<<<<<<<<<<<<<<  ✨ Codeium Command ⭐ >>>>>>>>>>>>>>>>
-// Assuming that you want to create a function to handle a command.
-// This function will take a command string as input and return an error if any occurs during handling.
+var handelers = map[string]func([]Value) Value{
+ "PING": ping,
 
-func handleCommand(cmd string) error {
-    // Implement command handling logic here.
-    // This is a placeholder, replace with actual logic.
-    switch cmd {
-    case "command1":
-        // Handle command1
-    case "command2":
-        // Handle command2
-    default:
-        return fmt.Errorf("Unknown command: %s", cmd)
-    }
-
-    return nil
 }
-<<<<<<<  559a7849-f67e-40f1-a4d9-6e42799f629c  >>>>>>>
+
+func ping(args []Value) Value{
+    return Value{typ: "string", str: "PONG"}
+}
